05dad5082e8750ac2b4e85a809c566a6a8266166
Abhishek Gupta <91587690+iamabhigupta@users.noreply.github.com>
2023-08-10 17:59:13 +0530

    font change

--- pages/_document.js
+++ pages/_document.js
@@ -1,14 +1,14 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html>
       <Head />
-      {/* <link rel="stylesheet" href="https://use.typekit.net/sqw6afh.css" /> */}
-      <link
+      <link rel="stylesheet" href="https://use.typekit.net/sqw6afh.css" />
+      {/* <link
         href="https://fonts.googleapis.com/css2?family=Reem+Kufi+Fun:wght@400;500;600;700&display=swap"
         rel="stylesheet"
-      />
+      /> */}
       <body>
         <Main />
         <NextScript />
